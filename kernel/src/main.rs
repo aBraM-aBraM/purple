df1cf59e133da6217af44914bc1b44110b6c4f7d
abram <eyalandron@gmail.com>
2023-12-17 12:24:23 +0200

    add testing infrastructure

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,18 +1,32 @@
 #![no_std]
 #![no_main]
+#![reexport_test_harness_main = "test_main"] // set test entry point name
+#![feature(custom_test_frameworks)]
+#![test_runner(kernel::test_runner)]
 
-mod vga_buffer;
+use kernel::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
 use core::panic::PanicInfo;
 
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    println!("{}", _info);
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    kernel::test_panic_handler(info)
 }
